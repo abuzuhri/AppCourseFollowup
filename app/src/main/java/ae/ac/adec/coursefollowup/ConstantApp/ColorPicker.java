c6b0a5ce66687ab4250397cf45f3fac475ca26ab
jmaelagha <jmaelagha@gmail.com>
2015-04-02 03:03:10 +0300

    Jaffer 2/4

---
+++ app/src/main/java/ae/ac/adec/coursefollowup/ConstantApp/ColorPicker.java
@@ -0,0 +1,31 @@
+package ae.ac.adec.coursefollowup.ConstantApp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+
+import java.util.List;
+
+import ae.ac.adec.coursefollowup.R;
+
+/**
+ * Created by JMA on 4/2/2015.
+ */
+public class ColorPicker {
+    String[] colors;
+    Activity context;
+    public CustomColorDialogClass dialog;
+
+    public ColorPicker(Activity context, int screenWidth, int screenHeight,AdapterView.OnItemClickListener listener) {
+        this.context = context;
+        populateColorsList();
+        dialog = new CustomColorDialogClass(context, "Pick a Color", screenWidth, screenHeight, colors
+                ,listener);
+        dialog.show(context.getFragmentManager(), "jma");
+    }
+
+    private void populateColorsList() {
+        colors = context.getResources().getStringArray(R.array.colors);
+    }
+}
